e4cbfc40696faffa150f2d263053221cb535ce41
annavighi <vighianna@gmail.com>
2024-12-16 14:55:12 -0300

    Criado o CRUD da classe Produto

---
+++ farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,37 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "É obrigatório informar o produto!")
+	private String produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return produto;
+	}
+
+	public void setCategoria(String categoria) {
+		this.produto = categoria;
+	}
+
+}
