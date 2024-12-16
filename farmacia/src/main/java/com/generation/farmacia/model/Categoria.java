4355fb54e88918bb80c23415b0cb58c88723a16a
annavighi <vighianna@gmail.com>
2024-12-16 15:54:43 -0300

    CRUD atualizado com o relacionamento ManyToOne e OneToMany entre as entidades criado

--- farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
+++ farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
@@ -17,6 +24,19 @@ public class Categoria {
 	
 	@NotBlank(message = "É obrigatório informar a categoria!")
 	private String categoria;
+	
+	@OneToMany (fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties
+	private List<Produto> produto;
+
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 
 	public Long getId() {
 		return id;
--- farmacia/src/main/java/com/generation/farmacia/model/Produto.java
+++ farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,11 +1,17 @@
 package com.generation.farmacia.model;
 
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tb_produto")
@@ -17,6 +23,27 @@ public class Produto {
 	
 	@NotBlank(message = "É obrigatório informar o produto!")
 	private String produto;
+	
+	@NotBlank(message = "É obrigatório informar o fabricante!")
+	private String fabricante;
+	
+	@NotNull(message = "É obrigatório informar o valor!")
+	private BigDecimal preco;
+	
+	@NotNull(message = "É obrigatório informar o estoque!")
+	private int estoque;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 	public Long getId() {
 		return id;
@@ -26,12 +53,36 @@ public class Produto {
 		this.id = id;
 	}
 
-	public String getCategoria() {
+	public String getProduto() {
 		return produto;
 	}
 
-	public void setCategoria(String categoria) {
-		this.produto = categoria;
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
 	}
 
 }
